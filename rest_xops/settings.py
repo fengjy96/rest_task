a63f011e96d78f0461d1b73a338645fdaad02cbb
one <onerf@sina.com>
2019-05-17 13:05:14 +0800

    【优化】代码注解

--- rest_xops/settings.py
+++ rest_xops/settings.py
@@ -5,16 +5,17 @@ import os, sys, datetime
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# 密钥，用于 hash 算法
 SECRET_KEY = 'x^*!*=ao%3t2ay-0zw-21!&oz=%4gwu0=&*omt@zi%3t^dyd8!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# 是否开启 DEBUDG 模式（生产环境中需要关闭）
 DEBUG = True
 
+# 一个字符串列表，用来表示 Django 站点可以服务的 host/domain
+# 这是一个安全措施，用来预防 HTTP Host header attacks
 ALLOWED_HOSTS = ['*']
 
-# Application definition
-
+# 在 Django 项目中需要安装的应用列表
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -23,17 +24,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'channels',
-     # 跨域
+    # 应用：解决跨域问题
     'corsheaders',
+    # 应用：
     'django_filters',
+    # 应用：基于角色的访问权限控制
     'rbac',
+    # 应用：配置管理数据库
     'cmdb',
+    # 应用：部署
     'deployment',
+    # 应用：项目管理系统业务
     'business',
+    # 应用：积分
     'points',
+    # 应用：项目管理系统业务配置
     'configuration',
 ]
 
+# 中间件
 MIDDLEWARE = [
     # 跨域
     'corsheaders.middleware.CorsMiddleware',
@@ -72,9 +81,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rest_xops.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+# 数据库相关配置
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -124,7 +131,8 @@ JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=3),
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
 }
-# redis 设置
+
+## redis 数据库相关配置
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
@@ -146,26 +154,30 @@ ASGI_APPLICATION = "rest_xops.routing.application"
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
+# 语言默认是 en-us，这里改为中文
 LANGUAGE_CODE = 'zh-hans'
 
+# 一个字符串，用来指定存储到数据库中的时区
 TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
+# 数据库存储使用时间，设置为 True 的话，时间会被存为 UTC 的时间
 USE_TZ = False
 
+# 此处重载是为了使我们的 UserProfile 生效
 AUTH_USER_MODEL = 'rbac.UserProfile'
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# 静态文件存放路径
 STATIC_URL = '/static/'
 
+# 资源文件存放路径
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# yml配置文件存放的目录
+# yml 配置文件存放的目录
 YML_CONF_DIR = os.path.join(BASE_DIR, 'conf')
 
 # 部署管理工作区地址
@@ -174,6 +186,7 @@ WORKSPACE = '/tmp/workspace/'
 # 日志
 BASE_LOG_DIR = os.path.join(BASE_DIR, 'logs')
 
+# 日志配置
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -184,7 +197,6 @@ LOGGING = {
         'simple': {
             'format': '[%(levelname)s][%(asctime)s]%(message)s'
         },
-
     },
     'handlers': {
         'default': {
@@ -203,8 +215,7 @@ LOGGING = {
             'backupCount': 5,
             'formatter': 'standard',
             'encoding': 'utf-8',
-        }
-
+        },
     },
     'loggers': {
         'info': {
@@ -220,7 +231,6 @@ LOGGING = {
         'error': {
             'handlers': ['error'],
             'level': 'ERROR',
-        }
-    }
-
+        },
+    },
 }
