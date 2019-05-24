7a4e96f505aa7cfa57f76c214395f95ba252f6e5
pointworld <onerf@sina.com>
2019-05-24 09:40:35 +0800

    【优化】代码逻辑优化

--- apps/business/models/files.py
+++ apps/business/models/files.py
@@ -1,16 +1,16 @@
 from django.db import models
 from business.models.task import Task
-from datetime import datetime
 from business.models.steplog import StepLog, FeedBackLog
+
 
 class Files(models.Model):
     """
     文件模型
     """
-    task = models.ForeignKey(Task, null=True, blank=True,verbose_name='任务标识', on_delete=models.CASCADE)
-    steplog = models.ForeignKey(StepLog, null=True, blank=True,verbose_name='日志标识', on_delete=models.CASCADE)
-    name = models.CharField(default='',max_length=150, verbose_name='文件名称')
-    path = models.CharField(default='',max_length=300, verbose_name='文件路径')
+    task = models.ForeignKey(Task, null=True, blank=True, verbose_name='任务标识', on_delete=models.CASCADE)
+    steplog = models.ForeignKey(StepLog, null=True, blank=True, verbose_name='日志标识', on_delete=models.CASCADE)
+    name = models.CharField(default='', max_length=150, verbose_name='文件名称')
+    path = models.CharField(default='', max_length=300, verbose_name='文件路径')
     type = models.IntegerField(default=1, verbose_name='类型')
     is_active = models.IntegerField(default=1, verbose_name='是否激活')
     add_time = models.DateTimeField(auto_now_add=True, verbose_name='添加时间')
@@ -22,13 +22,14 @@ class Files(models.Model):
         verbose_name = '文件'
         verbose_name_plural = verbose_name
 
+
 class ProgressTexts(models.Model):
     """
     进度富文本模型
     """
-    steplog = models.ForeignKey(StepLog, null=True, blank=True,verbose_name='日志标识', on_delete=models.CASCADE)
+    steplog = models.ForeignKey(StepLog, null=True, blank=True, verbose_name='日志标识', on_delete=models.CASCADE)
     type = models.IntegerField(default=0, verbose_name='类型')
-    content = models.TextField(default='',max_length=80, verbose_name='内容')
+    content = models.TextField(default='', max_length=80, verbose_name='内容')
     is_active = models.IntegerField(default=1, verbose_name='是否激活')
     add_time = models.DateTimeField(auto_now_add=True, verbose_name='添加时间')
 
@@ -39,30 +40,29 @@ class ProgressTexts(models.Model):
         verbose_name = '富文本'
         verbose_name_plural = verbose_name
 
+
 class FeedBacks(models.Model):
     """
     反馈文件模型
     """
-    feedbacklog = models.ForeignKey(FeedBackLog, null=True, blank=True,verbose_name='日志文件标识', on_delete=models.CASCADE)
-    name = models.CharField(default='',max_length=150, verbose_name='文件名称')
-    path = models.CharField(default='',max_length=300, verbose_name='文件路径')
+    feedbacklog = models.ForeignKey(FeedBackLog, null=True, blank=True, verbose_name='日志文件标识', on_delete=models.CASCADE)
+    name = models.CharField(default='', max_length=150, verbose_name='文件名称')
+    path = models.CharField(default='', max_length=300, verbose_name='文件路径')
     is_active = models.IntegerField(default=1, verbose_name='是否激活')
     add_time = models.DateTimeField(auto_now_add=True, verbose_name='添加时间')
-
-    def __str__(self):
-        return self.file.name
 
     class Meta:
         verbose_name = '反馈'
         verbose_name_plural = verbose_name
 
+
 class FeedBackTexts(models.Model):
     """
     反馈富文本模型
     """
-    feedbacklog = models.ForeignKey(FeedBackLog, null=True, blank=True,verbose_name='日志文件标识', on_delete=models.CASCADE)
+    feedbacklog = models.ForeignKey(FeedBackLog, null=True, blank=True, verbose_name='日志文件标识', on_delete=models.CASCADE)
     type = models.IntegerField(default=0, verbose_name='类型')
-    content = models.TextField(default='',max_length=80, verbose_name='内容')
+    content = models.TextField(default='', max_length=80, verbose_name='内容')
     is_active = models.IntegerField(default=1, verbose_name='是否激活')
     add_time = models.DateTimeField(auto_now_add=True, verbose_name='添加时间')
 
@@ -71,4 +71,3 @@ class FeedBackTexts(models.Model):
 
     class Meta:
         verbose_name = '反馈富文本'
-
--- apps/configuration/models.py
+++ apps/configuration/models.py
@@ -3,11 +3,49 @@ from django.db import models
 from rbac.models import Company, UserProfile
 
 
+class ProjectStatus(models.Model):
+    """
+    项目状态
+    """
+    index = models.IntegerField(verbose_name='排序序号')
+    num = models.IntegerField(verbose_name='真实序号')
+    key = models.CharField(max_length=30, verbose_name='状态名')
+    value = models.CharField(max_length=30, verbose_name='状态值')
+    text = models.CharField(max_length=30, verbose_name='状态值')
+    desc = models.CharField(max_length=50, null=True, blank=True, verbose_name='状态描述')
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = '项目状态'
+        verbose_name_plural = verbose_name
+
+
+class TaskStatus(models.Model):
+    """
+    任务状态
+    """
+    index = models.IntegerField(verbose_name='序号')
+    num = models.IntegerField(verbose_name='真实序号')
+    key = models.CharField(max_length=30, verbose_name='状态名')
+    value = models.CharField(max_length=30, verbose_name='状态值')
+    text = models.CharField(max_length=30, verbose_name='状态值')
+    desc = models.CharField(max_length=50, null=True, blank=True, verbose_name='状态描述')
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = '任务状态'
+        verbose_name_plural = verbose_name
+
+
 class TaskType(models.Model):
     """
     任务类型
     """
-    company = models.ForeignKey(Company, verbose_name='公司标识', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, null=True, blank=True, verbose_name='公司标识', on_delete=models.CASCADE)
     name = models.CharField(max_length=30, verbose_name='类型名称')
     index = models.IntegerField(verbose_name='类型序号')
     is_active = models.IntegerField(verbose_name='是否激活')
@@ -24,7 +62,7 @@ class TaskPriority(models.Model):
     """
     任务优先级模型
     """
-    company = models.ForeignKey(Company, verbose_name='公司标识', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, null=True, blank=True, verbose_name='公司标识', on_delete=models.CASCADE)
     name = models.CharField(max_length=30, verbose_name='优先级名称')
     index = models.IntegerField(verbose_name='优先级序号')
     weight = models.FloatField(verbose_name='权重')
@@ -43,7 +81,7 @@ class TaskQuality(models.Model):
     """
     任务品质要求表模型
     """
-    company = models.ForeignKey(Company, verbose_name='公司标识', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, null=True, blank=True, verbose_name='公司标识', on_delete=models.CASCADE)
     name = models.CharField(max_length=30, verbose_name='品质要求名称')
     index = models.IntegerField(verbose_name='品质要求序号')
     weight = models.FloatField(verbose_name='权重')
@@ -64,7 +102,7 @@ class TaskAssessment(models.Model):
     """
     name = models.CharField(max_length=20, verbose_name='评级名称')
     index = models.IntegerField(verbose_name='评级序号')
-    company = models.ForeignKey(Company, verbose_name='公司标识', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, null=True, blank=True, verbose_name='公司标识', on_delete=models.CASCADE)
     weight = models.FloatField(verbose_name='权重')
 
     is_active = models.IntegerField(verbose_name='是否激活')
@@ -81,7 +119,7 @@ class TaskDesignType(models.Model):
     """
     任务设计方式
     """
-    company = models.ForeignKey(Company, verbose_name='公司标识', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, null=True, blank=True, verbose_name='公司标识', on_delete=models.CASCADE)
     task_type = models.ForeignKey(TaskType, verbose_name='任务类型标识', on_delete=models.CASCADE)
     name = models.CharField(max_length=80, verbose_name='设计方式名称')
     index = models.IntegerField(verbose_name='设计方式序号')
@@ -100,7 +138,7 @@ class TaskStep(models.Model):
     """
     任务步骤表模型
     """
-    company = models.ForeignKey(Company, verbose_name='公司标识', on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, null=True, blank=True, verbose_name='公司标识', on_delete=models.CASCADE)
     name = models.CharField(max_length=80, verbose_name='任务步骤名称')
     task_type = models.ForeignKey(TaskType, verbose_name='任务类型标识', on_delete=models.CASCADE)
     task_design_type = models.ForeignKey(TaskDesignType, verbose_name='任务设计方式标识', on_delete=models.CASCADE)
