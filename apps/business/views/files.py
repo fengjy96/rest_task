6ddac4e7e866aae122f2b32cfd50606307cb992a
mykeyvi <4705057@qq.com>
2019-05-28 17:53:20 +0800

    add progress update

---
+++ apps/business/views/files.py
@@ -0,0 +1,325 @@
+from rest_framework import views
+from utils.basic import MykeyResponse
+from rest_framework import status
+from django.http import FileResponse
+from business.models.files import Files, ProgressTexts, FeedBacks, FeedBackTexts
+from rest_framework.viewsets import ModelViewSet
+from business.serializers.file_serializer import FilesSerializer, FilesListSerializer
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from business.filters import FilesFilter
+# from business.views.forms import UploadFileForm
+from rest_framework.generics import ListAPIView
+from django.conf import settings
+from business.models.steplog import StepLog, FeedBackLog, TaskLog
+import os
+import uuid
+
+
+class FilesViewSet(ModelViewSet):
+    """
+    文件：增删改查
+    """
+    queryset = Files.objects.all()
+    serializer_class = FilesSerializer
+    # permission_classes = (IsAuthenticated,)
+
+
+class FilesListViewSet(ListAPIView):
+    """
+    文件：增删改查
+    """
+    queryset = Files.objects.all()
+    serializer_class = FilesListSerializer
+    # permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    # 指定筛选类
+    filter_class = FilesFilter
+    ordering_fields = ('id',)
+
+    def get_queryset(self):
+        # 文件状态为激活
+        is_active = 1
+
+        return Files.objects.filter(is_active=is_active)
+
+
+class UploadFilesView(views.APIView):
+    """
+    上传单个或者多个文件
+    """
+
+    def post(self, request):
+        try:
+            # 获取用户上传的文件,保存到服务器,再添加到数据库
+            files = request.FILES.getlist('file')
+            # 判断文件列表是否存在文件
+            if len(files) > 0:
+                # 判断上传路径是否存在，不存在则创建
+                if not os.path.exists(settings.MEDIA_ROOT):
+                    os.makedirs(settings.MEDIA_ROOT)
+
+                # 遍历用户上传的文件列表
+                upload_files = []
+                dict_obj = {}
+                for file in files:
+
+                    # 获取文件反缀名
+                    extension = os.path.splitext(file.name)[1]
+                    # 通过uuid重命名上传的文件
+                    filename = '{}{}'.format(uuid.uuid4(), extension)
+                    # 构建文件路径
+                    file_path = '{}/{}'.format(settings.MEDIA_ROOT, filename)
+                    # 将上传的文件路径存储到upload_files中
+                    # 注意这样要构建相对路径MEDIA_URL+filename,这里可以保存到数据库
+                    dict_obj["file_name"] = filename
+                    dict_obj["file_path"] = file_path
+
+                    upload_files.append(dict_obj)
+                    # 保存文件
+                    with open(file_path, 'wb') as f:
+                        for c in file.chunks():
+                            f.write(c)
+                        f.close()
+
+                data = {'files': upload_files}
+
+                return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功', data=data)
+
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+
+
+class DeleteFileView(views.APIView):
+    """
+    删除单个文件或者多个文件
+    """
+
+    def post(self, request):
+        try:
+            # 获取文件名称列表
+            urls = request.POST.getlist('url')
+            # 判断是否存在文件列表
+            if len(urls) > 0:
+                # 判断文件径是否存在，存在则删除
+                for url in urls:
+                     if os.path.exists(url):
+                        os.remove(url)
+
+                return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功')
+
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+
+
+class AddTaskLogFiles(views.APIView):
+    """
+    上传多个文件
+    """
+
+    def post(self, request):
+        try:
+            # 任务标识
+            task_id = request.data.get('task_id')
+            # 标题
+            title = request.data.get('title')
+            # 备注
+            memo = request.data.get('memo')
+
+            # 增加步骤日志
+            if task_id is not None and title is not None and memo is not None:
+                tasklog = TaskLog(task_id=task_id, title=title, memo=memo)
+                tasklog.save()
+
+                # 获取用户上传的文件,保存到服务器,再添加到数据库
+                files = request.FILES.getlist('file')
+                # 判断文件列表是否存在文件
+                if len(files) > 0:
+                    # 判断上传路径是否存在，不存在则创建
+                    if not os.path.exists(settings.MEDIA_ROOT):
+                        os.makedirs(settings.MEDIA_ROOT)
+                    # 遍历用户上传的文件列表
+                    upload_files = []
+                    for file in files:
+                        # 获取文件反缀名
+                        extension = os.path.splitext(file.name)[1]
+                        # 通过uuid重命名上传的文件
+                        filename = '{}{}'.format(uuid.uuid4(), extension)
+                        # 构建文件路径
+                        file_path = '{}/{}'.format(settings.MEDIA_ROOT, filename)
+                        # 将上传的文件路径存储到upload_files中
+                        # 注意这样要构建相对路径MEDIA_URL+filename,这里可以保存到数据库
+                        upload_files.append('{}{}'.format(settings.MEDIA_URL, filename))
+                        # 保存文件
+                        with open(file_path, 'wb') as f:
+                            for c in file.chunks():
+                                f.write(c)
+                            f.close()
+
+                        # 增加文件
+                        file_ = Files(tasklog_id=tasklog.id, name=filename, path=file_path)
+                        file_.save()
+
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+
+        return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功')
+
+
+class AddStepLogFiles(views.APIView):
+    """
+    上传多个文件
+    """
+
+    def post(self, request):
+        try:
+            # 步骤标识
+            step_id = request.data.get('step_id')
+            # 标题
+            title = request.data.get('title')
+            # 进度
+            progress = request.data.get('progress')
+            # 备注
+            memo = request.data.get('memo')
+            # 类型
+            type = request.data.get('type')
+            # 内容
+            content = request.data.get('content')
+
+            # 增加步骤日志
+            if step_id is not None and title is not None and progress is not None and memo is not None:
+                steplog = StepLog(step_id=step_id, title=title, progress=progress, memo=memo)
+                steplog.save()
+
+                # 如果存在富文本,则先添加富文本
+                if type is not None and content is not None:
+                    if type == 0:
+                        progresstexts = ProgressTexts(steplog_id=steplog.id, content=content)
+                        progresstexts.save()
+
+                # 获取用户上传的文件,保存到服务器,再添加到数据库
+                files = request.FILES.getlist('file')
+                # 判断文件列表是否存在文件
+                if len(files) > 0:
+                    # 判断上传路径是否存在，不存在则创建
+                    if not os.path.exists(settings.MEDIA_ROOT):
+                        os.makedirs(settings.MEDIA_ROOT)
+                    # 遍历用户上传的文件列表
+                    upload_files = []
+                    for file in files:
+                        # 获取文件反缀名
+                        extension = os.path.splitext(file.name)[1]
+                        # 通过uuid重命名上传的文件
+                        filename = '{}{}'.format(uuid.uuid4(), extension)
+                        # 构建文件路径
+                        file_path = '{}/{}'.format(settings.MEDIA_ROOT, filename)
+                        # 将上传的文件路径存储到upload_files中
+                        # 注意这样要构建相对路径MEDIA_URL+filename,这里可以保存到数据库
+                        upload_files.append('{}{}'.format(settings.MEDIA_URL, filename))
+                        # 保存文件
+                        with open(file_path, 'wb') as f:
+                            for c in file.chunks():
+                                f.write(c)
+                            f.close()
+
+                        # 增加文件
+                        file_ = Files(steplog_id=steplog.id, name=filename, path=file_path)
+                        file_.save()
+
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+
+        return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功')
+
+
+class AddFeedBackLogFiles(views.APIView):
+    """
+    上传多个文件
+    """
+
+    def post(self, request):
+        try:
+            # 步骤标识
+            file_id = request.data.get('file_id')
+            # 标题
+            title = request.data.get('title')
+            # 备注
+            memo = request.data.get('memo')
+            # 类型
+            type = request.data.get('type')
+            # 内容
+            content = request.data.get('content')
+
+            # 增加反馈日志
+            if file_id is not None and title is not None and memo is not None:
+                feedbacklog = FeedBackLog(file_id=file_id, title=title, memo=memo)
+                feedbacklog.save()
+
+                # 如果存在富文本,则先添加富文本
+                if type is not None and content is not None:
+                    if type == 0:
+                        feedbacktexts = FeedBackTexts(feedbacklog_id=feedbacklog.id, content=content)
+                        feedbacktexts.save()
+
+                # 获取用户上传的文件,保存到服务器,再添加到数据库
+                files = request.FILES.getlist('file')
+                # 判断文件列表是否存在文件
+                if len(files) > 0:
+                    # 判断上传路径是否存在，不存在则创建
+                    if not os.path.exists(settings.MEDIA_ROOT):
+                        os.makedirs(settings.MEDIA_ROOT)
+                    # 遍历用户上传的文件列表
+                    upload_files = []
+                    for file in files:
+                        # 获取文件反缀名
+                        extension = os.path.splitext(file.name)[1]
+                        # 通过uuid重命名上传的文件
+                        filename = '{}{}'.format(uuid.uuid4(), extension)
+                        # 构建文件路径
+                        file_path = '{}/{}'.format(settings.MEDIA_ROOT, filename)
+                        # 将上传的文件路径存储到upload_files中
+                        # 注意这样要构建相对路径MEDIA_URL+filename,这里可以保存到数据库
+                        upload_files.append('{}{}'.format(settings.MEDIA_URL, filename))
+                        # 保存文件
+                        with open(file_path, 'wb') as f:
+                            for c in file.chunks():
+                                f.write(c)
+                            f.close()
+
+                        # 增加反馈文件
+                        feedbacks_ = FeedBacks(feedbacklog_id=feedbacklog.id, name=filename, path=file_path)
+                        feedbacks_.save()
+
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+
+        return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功')
+
+
+class FileDownloadView(views.APIView):
+    # 下载文件
+    def get(self, request):
+        filename = request.data.get('filename')
+        filepath_ = os.path.abspath(os.path.join(os.getcwd(), 'uploadfiles')) + '/' + filename
+        if os.path.isfile(filepath_):
+            pass
+
+        # 下载文件
+        def readFile(fn, buf_size=262144):  # 大文件下载，设定缓存大小
+            f = open(fn, "rb")
+            while True:  # 循环读取
+                c = f.read(buf_size)
+                if c:
+                    yield c
+                else:
+                    break
+            f.close()
+
+        from django.utils.http import urlquote
+
+        file = open(filepath_, 'rb')
+        response = FileResponse(file)
+        response['content_type'] = 'APPLICATION/OCTET-STREAM'
+        response['Content-Disposition'] = 'attachment; filename=' + urlquote(filename)  # 设定传输给客户端的文件名称
+        response['Content-Length'] = os.path.getsize(filepath_)  # 传输给客户端的文件大小
+        return response
---
+++ apps/business/views/step.py
@@ -0,0 +1,467 @@
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+from business.models.step import Step
+from business.serializers.step_serializer import StepSerializer, StepListSerializer, StepCreateSerializer
+from rest_framework.filters import OrderingFilter, SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from business.filters import StepFilter
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from business.models.files import Files, FeedBacks, ProgressTexts, FeedBackTexts
+from business.models.steplog import StepLog, FeedBackLog
+from common.custom import CommonPagination
+from utils.basic import MykeyResponse
+import json
+
+class StepViewSet(ModelViewSet):
+    """
+    任务步骤：增删改查
+    """
+    queryset = Step.objects.all()
+    serializer_class = StepSerializer
+    ordering_fields = ('id',)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    # 指定筛选类
+    filter_class = StepFilter
+    # 指定分页类
+    pagination_class = CommonPagination
+    # 指定授权类
+    permission_classes = (IsAuthenticated,)
+    # 指定认证类
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+    def get_serializer_class(self):
+        """
+        根据请求类型动态变更 serializer
+        :return:
+        """
+        if self.action == 'create':
+            return StepCreateSerializer
+        elif self.action == 'list':
+            return StepListSerializer
+        return StepSerializer
+
+    def create(self, request, *args, **kwargs):
+        # request.data['sender'] = request.user.id
+        # if request.data['receiver']:
+        #     request.data['receive_status'] = 1
+        # else:
+        #     request.data['receive_status'] = 0
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        # if request.data['receiver']:
+        #     request.data['receive_status'] = 1
+        # else:
+        #     request.data['receive_status'] = 0
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
+class StepListView(ListAPIView):
+    """
+    任务步骤：增删改查
+    """
+    queryset = Step.objects.all()
+    serializer_class = StepListSerializer
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    # 指定筛选类
+    filter_class = StepFilter
+    ordering_fields = ('id',)
+
+    def get_queryset(self):
+        # 步骤状态值为激活
+        is_active = 1
+
+        return Step.objects.filter(is_active=is_active)
+
+
+class StepLogFileFeedBacksView(APIView):
+    """
+    单个文件以及富文本预览以及查看反馈
+    """
+
+    def get(self, request, format=None):
+        try:
+            # 文件标识
+            file_id = request.data.get('file_id')
+            # 类型
+            type = request.data.get('type')
+
+            if type == 0:
+                file_objects_data = one_file_objects(file_id)
+            else:
+                file_objects_data = one_progresstext_objects(file_id)
+
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+        return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功', data=file_objects_data)
+
+
+class StepsLogsView(APIView):
+    """
+    查询单条步骤以及日志
+    """
+
+    def get(self, request, format=None):
+        try:
+            # 步骤标识
+            step_id = request.data.get('step_id')
+
+            step_objects_data = steplog_objects(step_id)
+
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+        return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功', data=step_objects_data)
+
+
+class StepsViewSet(APIView):
+    """
+    查询所有步骤以及日志
+    """
+
+    def get(self, request, format=None):
+        # 任务标识
+        try:
+            task_id = request.data.get('task_id')
+
+            step_objects_data = step_objects(task_id)
+        except Exception as e:
+            return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+        return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功', data=step_objects_data)
+
+
+class StepProgressUpdateView(APIView):
+    """
+    步骤进度更新
+    """
+
+    def post(self, request):
+        try:
+         # 步骤标识
+         step_id = request.data.get('step_id')
+         # 标题
+         title = request.data.get('title')
+         # 进度
+         progress = request.data.get('progress')
+         # 备注
+         memo = request.data.get('memo')
+         # 类型
+         type = request.data.get('type')
+         # 内容
+         content = request.data.get('content')
+
+         urls = request.data.get('url')
+
+         # 增加步骤日志
+         if step_id is not None and title is not None and progress is not None and memo is not None:
+             steplog = StepLog(step_id=step_id, title=title, progress=progress, memo=memo)
+             steplog.save()
+
+             if urls:
+                 for url in urls:
+                     #增加文件表记录
+                     file = Files(steplog=steplog,name=url['name'],path=url['url'])
+                     file.save()
+
+             # 如果存在富文本,则先添加富文本
+             if type is not None and content is not None:
+                 if type > 0:
+                     progresstexts = ProgressTexts(steplog=steplog,content=content)
+                     progresstexts.save()
+
+             # 更新步骤表时进度
+             step = Step.objects.get(id=step_id)
+             if step:
+                 step.progress = progress
+                 step.save()
+
+        except Exception as e:
+           return MykeyResponse(status=status.HTTP_400_BAD_REQUEST, msg='请求失败')
+
+        return MykeyResponse(status=status.HTTP_200_OK, msg='请求成功')
+
+def step_objects(task_id):
+    """
+    步骤
+    """
+    step_objects_data = []
+
+    steps = Step.objects.filter(task_id=task_id, is_active=1)
+    for step in steps:
+        if step:
+            dict_obj = {}
+            dict_obj["id"] = step.id
+
+            dict_obj_company = {}
+            if step.company:
+                dict_obj_company["id"] = step.company.id
+                dict_obj_company["name"] = step.company.name
+
+            dict_obj_task = {}
+            if step.task:
+                dict_obj_task["id"] = step.task.id
+                dict_obj_task["name"] = step.task.name
+
+            dict_obj_design_type = {}
+            if step.task_design_type:
+                dict_obj_design_type["id"] = step.task_design_type.id
+                dict_obj_design_type["name"] = step.task_design_type.name
+
+            dict_obj_sender = {}
+            if step.sender:
+                dict_obj_sender["id"] = step.sender.id
+                dict_obj_sender["name"] = step.sender.name
+
+            dict_obj_receiver = {}
+            if step.receiver:
+                dict_obj_receiver["id"] = step.receiver.id
+                dict_obj_receiver["name"] = step.receiver.name
+
+            dict_obj_auditor = {}
+            if step.auditor:
+                dict_obj_auditor["id"] = step.auditor.id
+                dict_obj_auditor["name"] = step.auditor.name
+
+            dict_obj["company"] = dict_obj_company
+            dict_obj["task_id"] = dict_obj_task
+            dict_obj["name"] = step.name
+
+            if step.index:
+                dict_obj["index"] = step.index
+            else:
+                dict_obj["index"] = ''
+
+            dict_obj["progress"] = step.progress
+            dict_obj["task_design_type"] = dict_obj_design_type
+            dict_obj["is_active"] = step.is_active
+            dict_obj["is_finished"] = step.is_finished
+            dict_obj["sender"] = dict_obj_sender
+            dict_obj["send_status"] = step.send_status
+            dict_obj["receiver"] = dict_obj_receiver
+            dict_obj["receive_status"] = step.receive_status
+            dict_obj["auditor"] = dict_obj_auditor
+            dict_obj["audit_status"] = step.audit_status
+
+            dict_obj["logs"] = steplogs_objects(step.id)
+
+            step_objects_data.append(dict_obj)
+
+    return step_objects_data
+
+
+def steplogs_objects(id):
+    """
+    时间轴日志列表
+    """
+    steplog_objects_data = []
+
+    steplogs = StepLog.objects.filter(step_id=id)
+    if steplogs:
+        for steplog in steplogs:
+            if steplog:
+                dict_obj = {}
+                dict_obj["id"] = steplog.id
+                dict_obj["title"] = steplog.title
+                dict_obj["progress"] = steplog.progress
+                dict_obj["memo"] = steplog.memo
+                dict_obj["add_time"] = steplog.add_time
+
+                dict_obj["files"] = files_objects(steplog.id)
+
+                steplog_objects_data.append(dict_obj)
+
+    return steplog_objects_data
+
+
+def files_objects(id):
+    """
+    日志文件列表
+    """
+    file_objects_data = []
+
+    files = Files.objects.filter(steplog_id=id)
+    if files:
+        for file in files:
+            if file:
+                dict_obj = {}
+                dict_obj["id"] = file.id
+                dict_obj["name"] = file.name
+                dict_obj["path"] = file.path
+                dict_obj["type"] = 1
+                dict_obj["content"] = ''
+                dict_obj["add_time"] = file.add_time
+
+                dict_obj["feedbacks"] = feedbacklog_objects(file.id, 1)
+
+                file_objects_data.append(dict_obj)
+
+    progresstexts = ProgressTexts.objects.filter(steplog_id=id)
+    if progresstexts:
+        for progresstext in progresstexts:
+            if progresstext:
+                dict_obj = {}
+                dict_obj["id"] = progresstext.id
+                dict_obj["name"] = ''
+                dict_obj["path"] = ''
+                dict_obj["type"] = 0
+                dict_obj["content"] = progresstext.content
+                dict_obj["add_time"] = progresstext.add_time
+
+                dict_obj["feedbacks"] = feedbacklog_objects(progresstext.id, 0)
+
+                file_objects_data.append(dict_obj)
+
+    return file_objects_data
+
+
+def feedbacklog_objects(id, type):
+    """
+    反馈日志
+    """
+    feedbacklog_objects_data = []
+
+    FeedBackLogs = FeedBackLog.objects.filter(link_id=id, type=type)
+    if FeedBackLogs:
+        for feedbacklog in FeedBackLogs:
+            if feedbacklog:
+                dict_obj = {}
+                dict_obj["id"] = feedbacklog.id
+                dict_obj["title"] = feedbacklog.title
+                dict_obj["memo"] = feedbacklog.memo
+                dict_obj["add_time"] = feedbacklog.add_time
+
+                dict_obj["files"] = feedback_objects(feedbacklog.id)
+
+                feedbacklog_objects_data.append(dict_obj)
+
+    return feedbacklog_objects_data
+
+
+def feedback_objects(id):
+    """
+    反馈文件
+    """
+    feedback_objects_data = []
+
+    feedbacks = FeedBacks.objects.filter(feedbacklog_id=id)
+    if feedbacks:
+        for feedback in feedbacks:
+            if feedback:
+                dict_obj = {}
+                dict_obj["id"] = feedback.id
+                dict_obj["name"] = feedback.name
+                dict_obj["path"] = feedback.path
+                dict_obj["type"] = 1
+                dict_obj["content"] = ''
+                dict_obj["add_time"] = feedback.add_time
+
+                feedback_objects_data.append(dict_obj)
+
+    feedbacktexts = FeedBackTexts.objects.filter(feedbacklog_id=id)
+    if feedbacktexts:
+        for feedbacktext in feedbacktexts:
+            if feedbacktext:
+                dict_obj = {}
+                dict_obj["id"] = feedbacktext.id
+                dict_obj["name"] = ''
+                dict_obj["path"] = ''
+                dict_obj["type"] = 0
+                dict_obj["content"] = feedbacktext.content
+                dict_obj["add_time"] = feedbacktext.add_time
+
+                feedback_objects_data.append(dict_obj)
+
+    return feedback_objects_data
+
+
+def steplog_objects(id):
+    """
+    单条时间轴日志
+    """
+    steplog_objects_data = []
+
+    steplogs = StepLog.objects.filter(id=id)
+    if steplogs:
+        for steplog in steplogs:
+            if steplog:
+                dict_obj = {}
+                dict_obj["id"] = steplog.id
+                dict_obj["title"] = steplog.title
+                dict_obj["progress"] = steplog.progress
+                dict_obj["memo"] = steplog.memo
+                dict_obj["add_time"] = steplog.add_time
+
+                dict_obj["files"] = files_objects(steplog.id)
+
+                steplog_objects_data.append(dict_obj)
+
+    return steplog_objects_data
+
+
+def one_file_objects(id):
+    """
+    单个文件以及反馈日志
+    """
+    file_objects_data = []
+
+    files = Files.objects.filter(id=id)
+    if files:
+        for file in files:
+            if file:
+                dict_obj = {}
+                dict_obj["id"] = file.id
+                dict_obj["name"] = file.name
+                dict_obj["path"] = file.path
+                dict_obj["type"] = 1
+                dict_obj["content"] = ''
+                dict_obj["add_time"] = file.add_time
+
+                dict_obj["feedbacks"] = feedbacklog_objects(file.id, 1)
+
+                file_objects_data.append(dict_obj)
+
+    return file_objects_data
+
+
+def one_progresstext_objects(id):
+    """
+    单个文件以及反馈日志
+    """
+    file_objects_data = []
+
+    progresstexts = ProgressTexts.objects.filter(id=id)
+    if progresstexts:
+        for progresstext in progresstexts:
+            if progresstext:
+                dict_obj = {}
+                dict_obj["id"] = progresstext.id
+                dict_obj["name"] = ''
+                dict_obj["path"] = ''
+                dict_obj["type"] = 0
+                dict_obj["content"] = progresstext.content
+                dict_obj["add_time"] = progresstext.add_time
+
+                dict_obj["feedbacks"] = feedbacklog_objects(progresstext.id, 0)
+
+                file_objects_data.append(dict_obj)
+
+    return file_objects_data
