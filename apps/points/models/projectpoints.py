c07c8218836fec0c43ee34d989732ade6a544820
mykeyvi <4705057@qq.com>
2019-06-10 16:08:59 +0800

    modify project checkpass

--- apps/points/models/projectpoints.py
+++ apps/points/models/projectpoints.py
@@ -11,7 +11,7 @@ class ProjectPoints(models.Model):
     user = models.ForeignKey(to=UserProfile, null=True, blank=True, verbose_name='用户标识', on_delete=models.CASCADE, related_name='project_points_user_id')
     project = models.ForeignKey(to=Project, null=True, blank=True, verbose_name='项目标识', on_delete=models.CASCADE, related_name='project_points_project_id')
     task = models.ForeignKey(to=Task, null=True, blank=True, verbose_name='任务标识', on_delete=models.CASCADE, related_name='project_points_task_id')
-    role = models.ForeignKey(to=Role, null=True, blank=True, verbose_name='任务标识', on_delete=models.CASCADE,related_name='project_points_task_id')
+    role = models.ForeignKey(to=Role, null=True, blank=True, verbose_name='角色标识', on_delete=models.CASCADE,related_name='project_points_role_id')
     points = models.IntegerField(default=0, verbose_name='积分')
     type = models.IntegerField(default=0, verbose_name='积分类型')
     is_active = models.IntegerField(default=1, verbose_name='是否激活')
